01bdc0ed7fb2a5879917c425baff5be96719c6d6
Linus <linus.hasselkvist@gmail.com>
2025-03-25 18:08:36 +0100

    added 6 demo buttons

--- source/main.c
+++ source/main.c
@@ -11,9 +11,9 @@ typedef struct windowModel {
 
 typedef struct UIResources {
         TTF_Font *font_russo;
+        TTF_Font *russo_small;
         SDL_Color black, white, red, green, blue;
 } UIRes;
-
 
 int UI_Init(UIRes *res);
 int createButtons(Button *buttons, WM *wm, const UIRes ui_res);
@@ -23,31 +23,29 @@ int main(int argc, char **argv) {
         TTF_Init();
 
         WM wm = {.is_running = true};
-
         wm.win = SDL_CreateWindow("UI test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600*16/9, 600, 0);
         wm.rend = SDL_CreateRenderer(wm.win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
         UIRes ui_res;
         UI_Init(&ui_res);
 
-        Button buttons[1];
-        int button_count = sizeof(buttons) / sizeof(Button);
+        Button buttons[6];
         createButtons(buttons, &wm, ui_res);
+        int button_count = sizeof(buttons) / sizeof(Button);
 
         SDL_Event event;
+        int mouse_x, mouse_y;
         while(wm.is_running) {
                 while(SDL_PollEvent(&event)) {
                         if(event.type == SDL_QUIT) {
                                 wm.is_running = false;
                         }
                 }
-
-                int mouse_x, mouse_y;
                 SDL_GetMouseState(&mouse_x, &mouse_y);
 
-                for(int i = 0; i < button_count; i++)
-                        if(button_event(buttons[i], mouse_x, mouse_y)) 
-                                break;
+                for(int i = 0; i < button_count; i++) {
+                        button_event(buttons[i], mouse_x, mouse_y);
+                }
 
                 SDL_SetRenderDrawColor(wm.rend, 0,0,10,0);
                 SDL_RenderClear(wm.rend);
@@ -59,6 +57,7 @@ int main(int argc, char **argv) {
         }
 
         TTF_CloseFont(ui_res.font_russo);
+        TTF_CloseFont(ui_res.russo_small);
         for(int i = 0; i < button_count; i++)
                 destroyButton(buttons[i]);
 
@@ -71,6 +70,7 @@ int main(int argc, char **argv) {
 
 int UI_Init(UIRes *res) {
         res->font_russo = TTF_OpenFont("res/RussoOne-Regular.ttf", 24);
+        res->russo_small = TTF_OpenFont("res/RussoOne-Regular.ttf", 12);
 
         res->black = createColor(0, 0, 0, 255);
         res->white = createColor(255, 255, 255, 255);
@@ -82,9 +82,46 @@ int UI_Init(UIRes *res) {
 
 int createButtons(Button *buttons, WM *wm, const UIRes ui_res) {
         buttons[0] = createButton(
-                wm->rend, "Button", createRect(100,100,400,200), 
-                ui_res.white, ui_res.black, ui_res.font_russo
+                wm->rend, "Button 1", createRect(0,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
         );
-        if(buttons[0] == NULL) return 1;
+        if(buttons[0] == NULL) return 0;
         button_setColorsHovered(wm->rend, buttons[0], ui_res.black, ui_res.white);
+
+        buttons[1] = createButton(
+                wm->rend, "Button 2", createRect(80,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
+        );
+        if(buttons[1] == NULL) return 0;
+        button_setColorsHovered(wm->rend, buttons[1], ui_res.black, ui_res.white);
+
+        buttons[2] = createButton(
+                wm->rend, "Button 3", createRect(160,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
+        );
+        if(buttons[2] == NULL) return 0;
+        button_setColorsHovered(wm->rend, buttons[2], ui_res.black, ui_res.white);
+
+        buttons[3] = createButton(
+                wm->rend, "Button 4", createRect(240,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
+        );
+        if(buttons[3] == NULL) return 0;
+        button_setColorsHovered(wm->rend, buttons[3], ui_res.black, ui_res.white);
+
+        buttons[4] = createButton(
+                wm->rend, "Button 5", createRect(320,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
+        );
+        if(buttons[4] == NULL) return 0;
+        button_setColorsHovered(wm->rend, buttons[4], ui_res.black, ui_res.white);
+
+        buttons[5] = createButton(
+                wm->rend, "Button 6", createRect(400,0,80,20), 
+                ui_res.white, ui_res.black, ui_res.russo_small
+        );
+        if(buttons[5] == NULL) return 0;
+        button_setColorsHovered(wm->rend, buttons[5], ui_res.black, ui_res.white);
+
+        return 1;
 }
